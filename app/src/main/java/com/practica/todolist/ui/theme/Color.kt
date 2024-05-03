8f57f2b24cf8d40ac5469f8769d22ef0517b87a2
alexandrappe <00117222@uca.edu.sv>
2024-05-03 04:52:38 -0600

    Add theme, color and loginField

--- app/src/main/java/com/practica/todolist/ui/theme/Color.kt
+++ app/src/main/java/com/practica/todolist/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.practica.todolist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkPurple = Color(0xFF4B0082)
+val SoftPurple = Color(0xFF9370DB)
+val SoftPink = Color(0xFFEFB8C8)
+val BackgroundGray = Color(0xFFFDFDFD)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPrimaryGray = Color(0xFF21262c)
+val DarkBackgroundGray = Color(0xFF101d25)
